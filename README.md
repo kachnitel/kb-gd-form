f13d7a4c5f85eaa52b858d417884f86cf4d6b9d6
Ondrej Vana <vana.ondrej@gmail.com>
2018-04-02 23:11:02 -0700

    create upload data & url params

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# Configuration:
+# URL Parameters
+
+- **id**: The ID Value (Unique identifier, for instance the `campaign_id` value)
+- **pkname**: Name of the identifier (`campaign_id`)
+- **keyname**: Name of the value that's being changed (`importance` etc..)
+
+Example URL: `http://localhost:8000/form.html?id=1234&pkname=campaign_id&keyname=importance`
+
+# JSON Configuration:
+
+See `config.dist.json` for a structure of the config
 
 ## Type
 radio, input, select..
--- form.js
+++ form.js
@@ -1,7 +1,11 @@
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    return results[1] || 0;
+}
+
 // 1) render form
 function createForm() {
     $.getJSON( "./config.json", function( data ) {
-        console.log(data)
         if(data.type == "select") {
             // possibly add <datalist>
             var items = [];
@@ -11,6 +15,7 @@ function createForm() {
 
             $( "<select/>", {
                 "class": "select-list",
+                "id": "form-value",
                 html: items.join( "" )
             }).prependTo( "form#dynamicForm" );
         } else if(data.type == "input") {
@@ -24,9 +29,33 @@ function createForm() {
 
 // 2) upload to AWS
 function submit() {
-//     console.log($("form#dynamicForm")[0][0].value);
-    var formData = new FormData("form#dynamicForm");
-    console.log(FormData);
+    $.getJSON( "./config.json", function( data ) {
+        AWS.config.update({accessKeyId: data.aws.key, secretAccessKey: data.aws.secret});
+        var s3BucketName = data.aws.bucket;
+        var s3RegionName = data.aws.region;
+
+        var s3key = data.aws.prefix + $.urlParam('pkname') + '_' + $.urlParam('id') + '.csv';
+        // TODO header
+        var body = [
+            $.urlParam('pkname'),
+            $.urlParam('id'),
+            $.urlParam('keyname'),
+            $("#form-value").val()
+        ].join(',');
+        var file = new File([new Blob([body])], 'temp');
+console.log(file);
+
+        var s3 = new AWS.S3({params: {Bucket: s3BucketName, Region: s3RegionName}});
+        s3.putObject({Key: s3key, ContentType: "text/csv", Body: file},
+        function(err, data) {
+            if (data !== null) {
+                alert("1");
+            }
+            else {
+                alert("2");
+            }
+        });
+    });
 }
 
 $(document).ready(function() {
